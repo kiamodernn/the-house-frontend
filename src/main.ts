6f81c1cd580916e632a1fe00bc5ed8fa7b4d7e37
kiamodernn <30872707+kiamodernn@users.noreply.github.com>
2026-08-12 11:59:55 +0330

    boot CasinoDayli header interactions

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { initNotifications } from './modules/notifications.js';
 import { initFilters } from './modules/filters.js';
 import { initCoin } from './modules/coin.js';
 import { initHeroSlider } from './modules/hero-slider.js';
+import { initCasinoDayliHeader } from './modules/casinodayli-header.js';
 
 const boot = () => {
   document.documentElement.classList.add('js');
+  initCasinoDayliHeader();
   initNavigation();
   initNotifications();
   initFilters();
